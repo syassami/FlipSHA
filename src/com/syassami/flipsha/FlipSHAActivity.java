024b86ff2b07bd9530e5cbcb7c3e3dd86acec7da
Shayan Yassami <syassami@gmail.com>
2012-06-11 21:00:08 -0700

    first commit

---
+++ src/com/syassami/flipsha/FlipSHAActivity.java
@@ -0,0 +1,13 @@
+package com.syassami.flipsha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlipSHAActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
